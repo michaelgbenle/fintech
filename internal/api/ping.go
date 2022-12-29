2f7f8b48400700ecfbf5b44b5e4f6f9c5d765d59
michaelgbenle <michaelgbenle@gmail.com>
2022-12-29 01:36:29 +0100

    in progress

--- internal/api/ping.go
+++ internal/api/ping.go
@@ -1,8 +1,10 @@
 package api
 
+import "github.com/gin-gonic/gin"
+
 //PingHandler is for testing the connections
 func (u *HTTPHandler) PingHandler(c *gin.Context) {
-	data := &model.Student{}
+	data := "i'm ready"
 
 	// healthcheck
 	helpers.Response(c, "pong", 200, data, nil)
