d379b39beaf4156495e4c56e2739f5814f6be2e3
michaelgbenle <michaelgbenle@gmail.com>
2022-12-29 01:10:46 +0100

    in progress

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,15 @@
 package main
 
+func main() {
+	//Gets the environment variables
+	env := server.InitDBParams()
+
+	//Initializes the database
+	db, err := repository.Initialize(env.DbUrl)
+	if err != nil {
+		return
+	}
+
+	//Runs the app
+	server.Run(db, env.Port)
+}
---
+++ internal/repository/database.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	"github.com/michaelgbenle/fintech/internal/ports"
+)
+
+type Postgres struct {
+	DB *gorm.DB
+}
+
+//NewDB create/returns a new instance of our Database
+func NewDB(DB *gorm.DB) ports.Repository {
+	return &Postgres{
+		DB: DB,
+	}
+}
+
+//Initialize opens the database, creates jobs table if not created and populate it if its empty and returns a DB
+func Initialize(dbURI string) (*gorm.DB, error) {
+
+	conn, err := gorm.Open("postgres", dbURI)
+	if err != nil {
+		log.Fatal( err)
+
+	}
+
+	conn.AutoMigrate()
+
+	return conn, nil
+}
