fcea6ad0e9c49ab2fa54b63bc06076beb9add52f
michaelgbenle <michaelgbenle@gmail.com>
2022-12-29 01:41:50 +0100

    in progress

--- internal/api/signup.go
+++ internal/api/signup.go
@@ -1,1 +1,12 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/michaelgbenle/fintech/internal/helpers"
+)
+
+func (u *HTTPHandler) SignUpHandler(c *gin.Context) {
+
+	
+	helpers.Response(c, "account created successfully", 201, nil, nil)
+}
