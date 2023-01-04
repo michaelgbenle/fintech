e6a68b163c08ad1f0ec83e4982fd8219da15d25b
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 20:20:06 +0100

    in progres

--- internal/api/login.go
+++ internal/api/login.go
@@ -3,11 +3,12 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/michaelgbenle/fintech/internal/helpers"
+	"github.com/michaelgbenle/fintech/internal/models"
 )
 
 func (u *HTTPHandler) LoginHandler(c *gin.Context) {
+var loginRequest *models.LoginRequest
 
-	
 
 	helpers.Response(c, "login successful", 200, nil, nil)
 }
