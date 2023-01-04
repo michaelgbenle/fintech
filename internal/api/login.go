2acaa795b6b0958e06c5acaf0b4273f6ff55b2c3
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 20:30:45 +0100

    in progres

--- internal/api/login.go
+++ internal/api/login.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/michaelgbenle/fintech/internal/helpers"
+	"github.com/michaelgbenle/fintech/internal/middleware"
 	"github.com/michaelgbenle/fintech/internal/models"
 	"golang.org/x/crypto/bcrypt"
 )
