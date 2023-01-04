66e32c281dea1f55cb8e38ade98749d19e375cab
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 19:12:21 +0100

    in progres

--- internal/api/signup.go
+++ internal/api/signup.go
@@ -3,9 +3,17 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/michaelgbenle/fintech/internal/helpers"
+	"github.com/michaelgbenle/fintech/internal/models"
 )
 
 func (u *HTTPHandler) SignUpHandler(c *gin.Context) {
+	var user *models.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		helpers.Response(c, "error", 400, nil, []string{"invalid request"})
+		return
+	}
+
 
 	helpers.Response(c, "account created successfully", 201, nil, nil)
 }
