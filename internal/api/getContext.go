236e6a51de2cea447eee4fcd28524f10a0247455
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 20:58:20 +0100

    get context complete

---
+++ internal/api/getContext.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/michaelgbenle/fintech/internal/models"
+)
+
+func (u *HTTPHandler) GetStudentFromContext(c *gin.Context) (*models.User, error) {
+	contextUser, exists := c.Get("user")
+	if !exists {
+		return nil, fmt.Errorf("error getting user from context")
+	}
+	user, ok := contextUser.(*models.User)
+	if !ok {
+		return nil, fmt.Errorf("an error occurred")
+	}
+	return user, nil
+}
