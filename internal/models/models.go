08efaf2a3b0cf1d4e25958c759a35f2b6d8c67b9
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 18:50:55 +0100

    in progres

--- internal/models/models.go
+++ internal/models/models.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -25,13 +26,30 @@ func (m *Model) BeforeCreate(tx *gorm.DB) (err error) {
 
 type User struct {
 	Model
-	FirstName 	string	`json:"firstname"`
-	LastName  	string	`json:"lastname"`
-	Email     	string	`json:"email"`
-	Password  	string	`json:"password"`
-	Pin 	 	string	`json:"pin"`
+	FirstName 	string	`json:"first_name" binding:"required"`
+	LastName  	string	`json:"lastname" binding:"required"`
+	Email     	string	`json:"email" binding:"required" gorm:"unique"`
+	Password  	string	`json:"password" binding:"required"`
+	Pin 	 	string	`json:"pin" binding:"required"`
 	Wallet   	string	`json:"wallet"`
 
+}
+
+func (user *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
+func (user *User) HashPin() error {
+	hashedPin, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Pin = string(hashedPin)
+	return nil
 }
 
 type Blacklist struct {
