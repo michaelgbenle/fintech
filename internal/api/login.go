20c2082d96162ce9bd2de61571b213931295dcc6
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 20:24:13 +0100

    in progres

--- internal/api/login.go
+++ internal/api/login.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/michaelgbenle/fintech/internal/helpers"
 	"github.com/michaelgbenle/fintech/internal/models"
@@ -13,6 +16,18 @@ if err != nil {
 	helpers.Response(c, "error", 400, nil, []string{"invalid request"})
 	return
 }
+
+//check if email exists
+user, userErr := u.Repository.FindUserByEmail(loginRequest.Email)
+if userErr != nil {
+	fmt.Println(userErr)
+	helpers.Response(c, "bad request", http.StatusBadRequest, nil, []string{"email does not exists"})
+	return
+}
+
+//check if password is correct
+
+
 
 
 	helpers.Response(c, "login successful", 200, nil, nil)
