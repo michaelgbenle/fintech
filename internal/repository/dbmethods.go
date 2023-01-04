5037fd0ddb43f5c5b2b8abb873f63a5cf57a9888
michaelgbenle <michaelgbenle@gmail.com>
2023-01-04 15:54:36 +0100

    in progres

--- internal/repository/dbmethods.go
+++ internal/repository/dbmethods.go
@@ -2,11 +2,10 @@ package repository
 
 import "github.com/michaelgbenle/fintech/internal/models"
 
-func (p *Postgres) FindStudentByEmail(email string) (*models.User, error) {
-	//var student *model.Student
-	student := &models.User{}
-	if err := p.DB.Where("email = ?", email).First(&student).Error; err != nil {
+func (p *Postgres) FindUserByEmail(email string) (*models.User, error) {
+	user := &models.User{}
+	if err := p.DB.Where("email = ?", email).First(&user).Error; err != nil {
 		return nil, err
 	}
-	return student, nil
+	return user, nil
 }
