eea45bc75f440ca2794e9510a0e457b7096d3295
michaelgbenle <michaelgbenle@gmail.com>
2023-01-06 20:34:07 +0100

    debit wallet in progress

--- internal/api/debit.go
+++ internal/api/debit.go
@@ -1,26 +1,15 @@
 package api
 
 import (
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/michaelgbenle/fintech/internal/helpers"
-	"github.com/michaelgbenle/fintech/internal/models"
 )
 
 func (u *HTTPHandler) DebitHandler(c *gin.Context) {
-	debiter, err := u.GetUserFromContext(c)
-	if err != nil {
-		helpers.Response(c, "Unauthorized", http.StatusUnauthorized, nil, []string{"unauthorized"})
-		return
-	}
+	
 
-	debit := &models.Money{}
-	err = c.ShouldBindJSON(&debit)
-	if err != nil {
-		helpers.Response(c, "error", 400, nil, []string{"invalid request"})
-		return
-	}
+
 
 	helpers.Response(c, "account debited successfully", 201, nil, nil)
 }
