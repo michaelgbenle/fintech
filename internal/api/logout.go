a4cebc80a67cd761ffa2c29440bbc4588bdee6f5
michaelgbenle <michaelgbenle@gmail.com>
2023-01-09 20:59:04 +0100

    get user and log out in progress

--- internal/api/logout.go
+++ internal/api/logout.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/michaelgbenle/fintech/internal/helpers"
 )
 
@@ -17,6 +18,25 @@ func (u *HTTPHandler) LogoutHandler(c *gin.Context) {
 	user, err := u.GetUserFromContext(c)
 	if err != nil {
 		helpers.Response(c, "error getting access token", http.StatusBadRequest, nil, []string{"bad request"})
+		return
+	}
+	token, err := jwt.ParseWithClaims(tokenstr, &middleware.Claims{}, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, fmt.Errorf("invalid signing algorithm")
+		}
+		return os.Getenv("JWT_SECRET"), nil
+	})
+
+	if claims, ok := token.Claims.(*middleware.Claims); !ok && !token.Valid {
+		helpers.JSON(c, "error inserting claims", http.StatusBadRequest, nil, []string{"bad request"})
+		return
+	} else {
+		claims.StandardClaims.ExpiresAt = time.Now().Add(-time.Hour).Unix()
+	}
+
+	err = u.UserService.AddTokenToBlacklist(foodBeneficiary.Email, tokenstr)
+	if err != nil {
+		helpers.JSON(c, "error inserting token into database", http.StatusInternalServerError, nil, []string{"Claims not valid type"})
 		return
 	}
 
