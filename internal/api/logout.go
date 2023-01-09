481b58b28b73d63d23e699bd1c05fb7a6fd66d96
michaelgbenle <michaelgbenle@gmail.com>
2023-01-09 20:56:48 +0100

    get user and log out in progress

--- internal/api/logout.go
+++ internal/api/logout.go
@@ -10,10 +10,17 @@ import (
 func (u *HTTPHandler) LogoutHandler(c *gin.Context) {
 	tokenstr, err := u.GetTokenFromContext(c)
 	if err != nil {
-		helpers.Response(c, "error getting access token", http.StatusBadRequest, nil, []string{"bad request"})
+		helpers.Response(c, "error getting access token", http.StatusInternalServerError, nil, []string{"internal error"})
+		return
+	}
+
+	foodBeneficiary, err := u.GetUserFromContext(c)
+	if err != nil {
+		helpers.JSON(c, "error getting access token", http.StatusBadRequest, nil, []string{"bad request"})
 		return
 	}
 
 
 	helpers.Response(c, "transactions", 201, nil, nil)
+
 }
